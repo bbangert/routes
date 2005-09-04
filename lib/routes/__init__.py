4cbcd0f4f88e5c787681f213269c012fb62a0919
bbangert <none@none>
2005-09-04 14:40:34 -0700

    [svn] Threading version of local() object in Python 2.4 works differently, now loads the right class for each version
    --HG--
    branch : trunk

--- lib/routes/__init__.py
+++ lib/routes/__init__.py
@@ -1,12 +1,21 @@
-import threadinglocal
+import threadinglocal, sys
 
-class _RequestConfig(object):
-    __shared_state = threadinglocal.local()
-    def __getattr__(self, name):
-        return self.__shared_state.__getattr__(name)
+if sys.version < '2.4':
+  class _RequestConfig(object):
+      __shared_state = threadinglocal.local()
+      def __getattr__(self, name):
+          return self.__shared_state.__getattr__(name)
 
-    def __setattr__(self, name, value):
-        return self.__shared_state.__setattr__(name, value)
+      def __setattr__(self, name, value):
+          return self.__shared_state.__setattr__(name, value)
+else:
+  class _RequestConfig(object):
+      __shared_state = threadinglocal.local()
+      def __getattr__(self, name):
+          return self.__shared_state.__getattribute__(name)
 
+      def __setattr__(self, name, value):
+          return self.__shared_state.__setattr__(name, value)
+  
 def request_config():
     return _RequestConfig()
