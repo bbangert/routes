bdb8ce395ef2765c2752a5b1e5b26a3a3862f69f
bbangert <none@none>
2005-09-18 12:46:16 -0700

    [svn] Prepping for 0.1 release
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -2,19 +2,22 @@ from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
 
-import sys
-import os
-from distutils.core import setup
-
 setup(name="Routes",
       version='0.1',
       description='Routing Recognition and Generation Tools',
       long_description="""
-A Routing package for Python that directly support 98% of the Rails unit tests
+A Routing package for Python that matches URL's to dicts and vice versa
 """,
+      classifiers=["Development Status :: 1 - Beta",
+                       "Intended Audience :: Web Framework Developers",
+                       "License :: OSI Approved :: New BSD License",
+                       "Programming Language :: Python",
+                       "Topic :: Internet :: WWW/HTTP",
+                       "Topic :: Internet :: WWW/HTTP :: URL Resolver",
+                       "Topic :: Software Development :: Libraries :: Python Modules",
+                       ],
       author='Ben Bangert',
       author_email='ben@groovie.org',
       url='http://routes.groovie.org/',
       packages=find_packages(exclude='tests'),
       )
-      
