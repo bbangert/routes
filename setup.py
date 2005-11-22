364f6c85276acf5353cd2c3325c65379663b1f37
bbangert <none@none>
2005-11-21 17:09:29 -0800

    [svn] Minor doc update, version update now that 1.0 is out
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name="Routes",
-      version='1.0',
+      version='1.1',
       description='Routing Recognition and Generation Tools',
       long_description="""
 A Routing package for Python that matches URL's to dicts and vice versa
