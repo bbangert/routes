27c28bc20d2c715b02c22857c1554be639c7dc4b
bbangert <none@none>
2006-09-17 10:48:33 -0700

    [svn] Bumping dev version to 1.5.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name="Routes",
-      version='1.4.2',
+      version='1.5',
       description='Routing Recognition and Generation Tools',
       long_description="""
 A Routing package for Python that matches URL's to dicts and vice versa
