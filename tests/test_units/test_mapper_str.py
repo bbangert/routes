516a748603ae6c95139040fed43f3db4cad3b499
Mike Burrows <mjb@asplake.co.uk>
2009-12-26 16:33:59 +0000

    add unit test for Mapper.__str__()
    --HG--
    branch : trunk

---
+++ tests/test_units/test_mapper_str.py
@@ -0,0 +1,18 @@
+import unittest
+from routes import Mapper
+
+class TestMapperStr(unittest.TestCase):
+    def test_str(self):
+        m = Mapper()
+        m.connect('/{controller}/{action}')
+        m.connect('entries', '/entries', controller='entry', action='index')
+        m.connect('entry', '/entries/{id}', controller='entry', action='show')
+
+        expected = """\
+Route name Methods Path
+                   {controller}/{action}
+entries            entries
+entry              entries/{id}"""
+        
+        for expected_line, actual_line in zip(expected.splitlines(), str(m).splitlines()):
+            assert expected_line == actual_line.rstrip()
