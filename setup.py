eeb33f42e5f7a7db0e40d60d920516f8d75702be
Ben Bangert <ben@groovie.org>
2017-01-01 11:22:39 -0800

    chore: add python 3.6 to tox/pypi vars

--- setup.py
+++ setup.py
@@ -42,7 +42,8 @@ setup(name="Routes",
                    "Programming Language :: Python :: 3.2",
                    "Programming Language :: Python :: 3.3",
                    "Programming Language :: Python :: 3.4",
-                   "Programming Language :: Python :: 3.5"
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6"
                    ],
       keywords='routes webob dispatch',
       author="Ben Bangert",
